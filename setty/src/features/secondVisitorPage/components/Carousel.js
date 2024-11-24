47a366c523dfb96bd1305ad005d22436aa0cdaa0
SungMin Yoon <yoonmin.tech@gmail.com>
2024-11-24 23:38:12 +0900

    style : update carousel icon

--- setty/src/features/secondVisitorPage/components/Carousel.js
+++ setty/src/features/secondVisitorPage/components/Carousel.js
@@ -4,9 +4,14 @@ import './ui-intro-page.css';
 
 
 const ImgCarousel = ({imgData}) => {
-  
+  const prevIcon ="https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/arrow_back_ios.svg";
+  const nextIcon="https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/arrow_forward_ios.svg";
+
   return (
-    <Carousel>
+    <Carousel 
+      prevIcon={<img src={prevIcon} className="custom-prev-icon" />} // 이전 버튼 아이콘 커스터마이즈
+      nextIcon={<img src={nextIcon} className="custom-next-icon" />} 
+    >
         {imgData.map((image, index) => (
             <Carousel.Item key={index}>
             <img
