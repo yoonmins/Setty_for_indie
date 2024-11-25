823a12f917253bd3bf892cb73f7e2a6684339601
SungMin Yoon <yoonmin.tech@gmail.com>
2024-11-25 10:54:32 +0900

    fix : change tab img & change section3 img &upload section 4

--- setty/src/features/secondVisitorPage/pages/SecondVisitorView.js
+++ setty/src/features/secondVisitorPage/pages/SecondVisitorView.js
@@ -53,7 +53,24 @@ const SecondView = () =>{
         }
     ]
     // 04 ui img data
-
+    const fourSectionCarousel =[
+        {
+            src:"https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/setty-second-page/ui_14.png", 
+            alt:"four img section-4"
+        },
+        {
+            src:"https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/setty-second-page/ui_12.png", 
+            alt:"four img section-2"
+        },
+        {
+            src:"https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/setty-second-page/ui_13.png", 
+            alt:"four img section-3"
+        },
+        {
+            src:"https://setty-hongik-2024.s3.ap-northeast-2.amazonaws.com/setty-second-page/ui_11.png", 
+            alt:"four img section-1"
+        },
+    ]
     return(
         <div className='ui-design-components'>
             <UiPageImage imgData={mainImg}/>
@@ -65,6 +82,7 @@ const SecondView = () =>{
             <UiIntroPageMainContaintForm titleNumber={"03"} titleText={"새로운 공연을 위한 기획"} showMusicIcon={true} showTouchIcon={true}/>
             <ImgCarousel imgData={thirdSectionCarousel} />
             <UiIntroPageMainContaintForm titleNumber={"04"} titleText={"창의적인 무대 생성"} showMusicIcon={true} showTouchIcon={true}/>
+            <ImgCarousel imgData={fourSectionCarousel} />
         </div>
     );
 }
